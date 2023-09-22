f547d990d6e30ad91f92b308f1a90d00465245a9
LanternD <yangdavid1028@gmail.com>
2023-09-22 16:34:22 -0700

    fixed bug in tag page; now it processes URL with hashtag correctly.

--- js/jquery.tagcloud.js
+++ js/jquery.tagcloud.js
@@ -1,35 +1,34 @@
 (function($) {
-
     $.fn.tagcloud = function(options) {
         var opts = $.extend({}, $.fn.tagcloud.defaults, options);
-        tagWeights = this.map(function() {
+        var tagWeights = this.map(function() {
             return $(this).attr("rel");
         });
         tagWeights = jQuery.makeArray(tagWeights).sort(compareWeights);
-        lowest = tagWeights[0];
-        highest = tagWeights.pop();
-        range = highest - lowest;
+        var lowest = tagWeights[0];
+        var highest = tagWeights.pop();
+        var range = highest - lowest;
         if (range === 0) {
             range = 1;
         }
         // Sizes
         if (opts.size) {
-            fontIncr = (opts.size.end - opts.size.start) / range;
+            var fontIncr = (opts.size.end - opts.size.start) / range;
         }
         // Colors
         if (opts.color) {
-            colorIncr = colorIncrement(opts.color, range);
+            var colorIncr = colorIncrement(opts.color, range);
         }
         return this.each(function() {
-            weighting = $(this).attr("rel") - lowest;
+            var weighting = $(this).attr("rel") - lowest;
             if (opts.size) {
                 $(this).css({
-                    "font-size": opts.size.start + (weighting * fontIncr) + opts.size.unit
+                    "font-size": opts.size.start + weighting * fontIncr + opts.size.unit,
                 });
             }
             if (opts.color) {
                 $(this).css({
-                    "color": tagColor(opts.color, colorIncr, weighting)
+                    color: tagColor(opts.color, colorIncr, weighting),
                 });
             }
         });
@@ -39,8 +38,8 @@
         size: {
             start: 14,
             end: 18,
-            unit: "pt"
-        }
+            unit: "pt",
+        },
     };
 
     // Converts hex to an RGB array
@@ -50,17 +49,24 @@
                 return el + el;
             }).join("");
         }
-        hex = /(\w{2})(\w{2})(\w{2})/.exec(code);
-        return [parseInt(hex[1], 16), parseInt(hex[2], 16), parseInt(hex[3], 16)];
+        var hex = /(\w{2})(\w{2})(\w{2})/.exec(code);
+        return [
+            parseInt(hex[1], 16),
+            parseInt(hex[2], 16),
+            parseInt(hex[3], 16),
+        ];
     }
 
     // Converts an RGB array to hex
     function toHex(ary) {
-        return "#" + jQuery.map(ary, function(i) {
-            hex = i.toString(16);
-            hex = (hex.length == 1) ? "0" + hex : hex;
-            return hex;
-        }).join("");
+        return (
+            "#" +
+            jQuery.map(ary, function(i) {
+                var hex = i.toString(16);
+                hex = hex.length == 1 ? "0" + hex : hex;
+                return hex;
+            }).join("")
+        );
     }
 
     function colorIncrement(color, range) {
@@ -70,8 +76,8 @@
     }
 
     function tagColor(color, increment, weighting) {
-        rgb = jQuery.map(toRGB(color.start), function(n, i) {
-            ref = Math.round(n + (increment[i] * weighting));
+        var rgb = jQuery.map(toRGB(color.start), function(n, i) {
+            var ref = Math.round(n + increment[i] * weighting);
             if (ref > 255) {
                 ref = 255;
             } else {
@@ -87,55 +93,67 @@
     function compareWeights(a, b) {
         return a - b;
     }
-
 })(jQuery);
 
 (function() {
-
     function tag_get_post(tag_name) {
         console.log(tag_name);
         $.ajax({
-                url: '/tags.json',
-                dataType: 'json',
+                url: "/tags.json",
+                dataType: "json",
             })
             .done(function(tag_post) {
                 wanted_post = tag_post.map(function(tag) {
-                    let name = tag.name,
+                    var name = tag.name,
                         post = tag.post;
                     if (name == tag_name) {
                         var tmpl = '<h3>属于该Tag的文章</h3>';
                         tmpl += '<ul class="listing" id="tag-post-list" >';
                         for (var i = 0; i < post.length; i++) {
-                            tmpl += '<li class="listing-item" style="list-style-type:none;"><time datetime="' + post[i].date + '">' + post[i].date + '</time>';
-                            tmpl += '<a href="' + post[i].url + '" title="' + post[i].title + '" class="listing-item-a">' + post[i].title + '</a></li>';
+                            tmpl +=
+                                '<li class="listing-item" style="list-style-type:none;"><time datetime="' +
+                                post[i].date +
+                                '">' +
+                                post[i].date +
+                                "</time>";
+                            tmpl +=
+                                '<a href="' +
+                                post[i].url +
+                                '" title="' +
+                                post[i].title +
+                                '" class="listing-item-a">' +
+                                post[i].title +
+                                "</a></li>";
                         }
-                        result_container = '#tag-post-container';
-                        if ($(result_container).has('ul .listing')) {
+                        result_container = "#tag-post-container";
+                        if ($(result_container).has("ul .listing")) {
                             $(result_container).empty();
                         }
 
                         $(result_container).append($(tmpl));
                         var scroll_num = $(result_container).offset().top;
-                        $('body, html').animate({
-                            scrollTop: scroll_num - 25
-                        }, 400, 'swing');
+                        $("body, html").animate({
+                                scrollTop: scroll_num - 25,
+                            },
+                            400,
+                            "swing"
+                        );
                     }
                 });
             });
-
     }
 
-    $('#tag_cloud a').click(function() {
+    $("#tag_cloud a").click(function() {
         var tag_name = this.title;
         tag_get_post(tag_name);
     });
 
-    if (window.location.hash != '') {
+    // If URL in tag page has a hash, process it and list related posts.
+    var hash = window.location.hash;
+    if (hash) {
         console.log("The path contains a tag, process it.");
-        var tag_name = window.location.hash.slice(1);
+        var targetTag = decodeURIComponent(hash.substring(1));
 
-        tag_get_post(tag_name);
-    } else {
-        console.log("it is a root path for the tag page, do nothing");
+        tag_get_post(targetTag);
     }
 })();
